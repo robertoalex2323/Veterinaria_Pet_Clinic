ee3cbc3996f84d44ef57138a464766d188058a58
robertoalex2323 <betodokij15@gmail.com>
2026-04-16 01:27:38 -0500

    actualización del login

--- src/main/resources/static/Js/Login.js
+++ src/main/resources/static/Js/Login.js
@@ -8,7 +8,7 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
   if (user === "admin" && pass === "1234") {
     msg.style.color = "green";
     msg.textContent = "Login correcto ✔";
-    alert("Bienvenido a VetCare");
+    alert("Bienvenido a Pet Clinic");
   } else {
     msg.style.color = "red";
     msg.textContent = "Usuario o contraseña incorrectos";
